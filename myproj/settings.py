2b21554b968feb5ce59cdf9ca2646f9e074d0273
ys <ys@betweak.com>
2023-10-26 16:36:33 +0900

    Update settings.py

--- myproj/settings.py
+++ myproj/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'myproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "HOST": "db",
+        "PORT": "3306",
+        "NAME": "django_proj",
+
+        "USER": "root",
+        "PASSWORD": "admin1234"
     }
 }
 
